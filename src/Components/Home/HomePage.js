bd991e818a6bf8a62631cc6dcea166083dcd2b23
Your Name <you@example.com>
2023-04-23 18:11:06 +0530

    HomePageupdated

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -51,19 +51,46 @@
 
 // export default Home;
 
-import React from "react";
-import heroImage from './../Home/strinX.png';
-import styles from "./HomePage.css";
+// import React from "react";
+// import heroImage from './../Home/strinX.png';
+// import styles from "./HomePage.css";
 
-function Home() {
+// function Home() {
+//   return (
+//     <div className={styles.container}>
+//       <div className="bgImage">
+//       <div className={styles.innerBox}>
+//         <img className={styles.image} src={heroImage} alt="home" />
+//         <h1 style={{ color: 'white'}}>Welcome to Guitar Chord Recognition </h1>
+//       </div>
+//     </div>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Home(props) {
   return (
-    <div className={styles.container}>
-      <div className="bgImage">
-      <div className={styles.innerBox}>
-        <img className={styles.image} src={heroImage} alt="home" />
-        <h1 style={{ color: 'white'}}>Welcome to Guitar Chord Recognition </h1>
+    <div>
+      <div>
+        <h1>
+          <Link to="/login">Login</Link>
+        </h1>
+        <br />
+        <h1>
+          <Link to="/signup">Signup</Link>
+        </h1>
       </div>
-    </div>
+
+      <br />
+      <br />
+      <br />
+
+      <h2>{props.name ? `Welcome - ${props.name}` : "Login please"}</h2>
     </div>
   );
 }
